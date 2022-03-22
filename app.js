bdeae770fc9996a60a528a36c80face601b2c3d1
Rahmat <oktrifianto@gmail.com>
2022-03-22 17:17:22 +0700

    Handling `404` route

--- app.js
+++ app.js
@@ -12,4 +12,17 @@ app.get('/', (req, res) => {
   })
 })
 
+/**
+ * @description   Handling 404 page
+ * @method        {all except defined}
+ * @path          *
+ * @return        404
+ */
+app.use("*", (req, res) => {
+  res.status(404).json({
+    "status"  : res.statusCode,
+    "message" : "Sorry, page not found"
+  })
+})
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
