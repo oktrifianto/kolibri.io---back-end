c40ec41c8fb40c98cd2dacca81b05baa99e6eb16
Rahmat <oktrifianto@gmail.com>
2022-03-22 22:16:55 +0700

    Test `sequelize` is success

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express   = require('express')
 const app       = express()
 const port      = 3000
+const db        = require('./config/db')
+
+try {
+  db.authenticate();
+  console.log('Connection has been established successfully!');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 const UserRoutes  = require('./routes/user.routes')
 app.use('/user', UserRoutes)
