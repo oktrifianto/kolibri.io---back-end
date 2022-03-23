b98144373f7bf2315414b302fae40ed695065e10
Rahmat <oktrifianto@gmail.com>
2022-03-23 14:04:11 +0700

    Add new `getUserByUsername` method in the controllers

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -12,6 +12,28 @@ const getAllUsers = async (req, res) => {
   }
 }
 
+const getUserByUsername = async (req, res) => {
+  try {
+    const { username } = req.params
+    const userData = await User.findOne({
+      attributes: [
+        'id',
+        'username'
+      ], // SELECT id, username FROM ...
+      where: {
+        username: `${username}`
+      } // WHERE username="" ...
+    })
+
+    res.status(200).json({
+      "status"  : res.statusCode,
+      "message" : userData
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  getUserByUsername
 }
