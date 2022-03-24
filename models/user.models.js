20fa49ba33377e2e49b7ccc043a8b7f1e79ae669
Rahmat <oktrifianto@gmail.com>
2022-03-24 08:30:00 +0700

    Fix: change some attributes

--- models/user.models.js
+++ models/user.models.js
@@ -5,15 +5,18 @@ const db  = require('../config/db')
  * How to define model?
  * sequelize.define('name', {attributes}, {options})
  * db.define('name', {attributes}, {options})
+ * @see https://github.com/sequelize/express-example/blob/master/express-main-example/sequelize/models/user.model.js
  */
 const User = db.define('Users', { // `Users` is [model name]
   username: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   email: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   password: {
     type: DataTypes.STRING,
