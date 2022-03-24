ab84d0dd8fb97ba3126dd755f619f96c8010535b
Rahmat <oktrifianto@gmail.com>
2022-03-24 14:10:15 +0700

    Add `getSingleProduct` method to controllers

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -25,6 +25,37 @@ const createOneProduct = async (req, res) => {
   }
 }
 
+/**
+ * Controller for get single product
+ * @path  /product/:id
+ */
+const getSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findOne({
+      where: {
+        id: id
+      }
+    })
+
+    if (product == null) {
+      res.status(404).json({
+        "status"  : res.statusCode,
+        "message" : "Product not found."
+      })
+    } else {
+      res.status(200).json({
+        "status"  : res.statusCode,
+        "message" : product
+      })
+    }
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 module.exports = {
-  createOneProduct
+  createOneProduct,
+  getSingleProduct
 }
