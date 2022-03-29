310f979cd5ef472c0f436f501322f7d57cc9280b
Rahmat <oktrifianto@gmail.com>
2022-03-29 13:08:35 +0700

    Add `loginUser` method to the user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -22,8 +22,16 @@ router.get('/:username', lib.getUserByUsername)
  * SignUp new user
  * @method    POST 
  * @path      /user/signup
- * @requires  {json body} -> email, password
+ * @requires  {json body} -> email, password, username
  */
 router.post('/signup', lib.signupNewUser)
+
+/**
+ * Login User
+ * @method    POST 
+ * @path      /user/login
+ * @requires  {json body} -> email, password
+ */
+router.post('/login', lib.loginUser)
 
 module.exports = router
